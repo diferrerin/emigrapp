63df6dd4c753dc457924f29fe916cb7f5299bc0f
Daniel Ferrerin <diferrerin@gmail.com>
2022-06-08 22:32:28 -0300

    Commit 7 - DOM y Eventos v3

--- assets/cuestionariov2.js
+++ assets/cuestionariov2.js
@@ -2,7 +2,7 @@ document.addEventListener(`DOMContentLoaded`,function(){
 //------------------------------------Inicio Script DOM+Events--------------------------------
 //------------------------------------CLASES--------------------------------------------------
 class Emigrante{
-    constructor(nombre, edad, origen , destino , familia, esUE, mascota, conductor, tipoVisa, resultado = []) {
+    constructor(nombre, edad, origen , destino , familia, esUE, mascota, conductor, tipoVisa, resultado) {
         this.nombre = nombre; //String
         this.edad   = edad; //int
         this.origen  = origen;// String
@@ -15,24 +15,39 @@ class Emigrante{
         this.resultado = resultado ;
     }
     crearResultado(){
-        return this.resultado = "Test Resultado: Nombre: "+this.nombre+" Edad: "+this.edad ;
+        return this.resultado = 
+        `Resultado: Nombre: ${this.nombre} 
+                    Edad: ${this.edad} 
+                    Origen: ${this.origen}
+                    Destino: ${this.destino}
+                    ` ;//Estado Civil: ${this.familia}
     }
 }
 //----------------------------------------------------FUNCIONES-----------------------------
 function generaEmigranteDom(){
     let resultadoe = new Emigrante();
-    resultadoe.nombre = document.getElementById( `nombre` ).value
-    resultadoe.edad = document.getElementById( `edad` ).value
+    //CHEQUEAR QUE ESTEN CARGADOS LOS DATOS
+    resultadoe.nombre = document.getElementById( `nombre` ).value;
+    resultadoe.edad = document.getElementById( `edad` ).value;
     //ver el resto de atributos 
+    resultadoe.origen = document.getElementById(`origen`).value;
+    resultadoe.destino = document.getElementById(`destino`).value;
+    //resultadoe.familia = document.getElementById(`estadocivil`).value;//No funciona, tira undefined
 
     return resultadoe;
 }
 
 function functSubmit(){
     let emigrante = new Emigrante(); 
-    emigrante = generaEmigranteDom();//loguear
+    emigrante = generaEmigranteDom();//Genera Emigrante en base al HTML
     console.log(emigrante.crearResultado());//ver rapido test
     alert(emigrante.resultado);
+    // genera respuesta DOM:
+    let respuestaDom = document.getElementById(`textoResultado`);
+    respuestaDom.innerText = emigrante.resultado;
+    console.log(respuestaDom);
+    //No logro evitar que refresque, para ver el resultado en el html.....
+
 }
 
 //----------------------------------------------------MAIN-----------------------------------
@@ -49,7 +64,6 @@ DOM:
 2- Modificar etiquetas existentes en función del resultado de operaciones.
 Eventos:
 1- Definición de un algoritmo en JavaScript que opere sobre el DOM manejando eventos.
-
 
 ----Guion para entrega
 --El usuario completa el formulario y acciona el boton Enviar (Evento)
